bbc74023526a2529506e31f2a6a5f60800464980
indahmutiah <indahmutiahmz@gmail.com>
2026-04-12 14:21:34 +0700

    fix: get env variable

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BASE_URL || 'http://emra.chat/login',
+    baseURL: process.env.BASE_URL || 'http://emra.chat',
     video: 'on',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -40,15 +40,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,20 +1,22 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login';
+import dotenv from 'dotenv';
+dotenv.config();
 
 test.describe('TS001-login', () => {
   test('TC001-Positive-Login Success', async ({ page }) => {
     const loginPage = new LoginPage(page);
-    await page.goto (process.env.BASE_URL || 'emra.chat/login');
-    await loginPage.email.fill(process.env.EMAIL || 'email@gmail.com');
-    await loginPage.password.fill(process.env.PASSWORD || 'password');
+    await page.goto ("/login");
+    await loginPage.email.fill(process.env.EMAIL!);
+    await loginPage.password.fill(process.env.PASSWORD!);
     await loginPage.loginButton.click();
     await expect(loginPage.welcomeMessage).toBeVisible();
   });
 
   test('TC002-Negative-Login Wrong Password', async ({ page }) => {
     const loginPage = new LoginPage(page);
-    await page.goto (process.env.BASE_URL || 'emra.chat/login');
-    await loginPage.email.fill(process.env.EMAIL || 'email@gmail.com');
+    await page.goto ("/login");
+    await loginPage.email.fill(process.env.EMAIL!);
     await loginPage.password.fill('pass123');
     await loginPage.loginButton.click();
     await expect(loginPage.errorMessage).toBeVisible();
@@ -22,9 +24,9 @@ test.describe('TS001-login', () => {
 
   test('TC003-Negative-Login Wrong email', async ({ page }) => {
     const loginPage = new LoginPage(page);
-    await page.goto (process.env.BASE_URL || 'emra.chat/login');
+    await page.goto ("/login");
     await loginPage.email.fill('wrong_user@gmail.com');
-    await loginPage.password.fill(process.env.PASSWORD || 'password');
+    await loginPage.password.fill(process.env.PASSWORD!);
     await loginPage.loginButton.click();
     await expect(loginPage.errorMessage).toBeVisible();
   });
